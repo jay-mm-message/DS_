c107f2a2a2f4259523c39f3ef271bebfef1abada
jay <jay.mm.message@gmail.com>
2023-07-28 16:24:55 +0800

    Refactor notation of parent

--- tree/arr_parent_def.c
+++ tree/arr_parent_def.c
@@ -1,12 +1,14 @@
 
+#define MAX_TREE_SIZE 100
+typedef int TElemType;
 typedef struct PTNode
 {
-  int data;   /* data */
+  TElemType data;   /* data */
   int parent; // parent location
 } PTNode;
 
 typedef struct
 {
-  PTNode node[100];
+  PTNode node[MAX_TREE_SIZE];
   int r, n;  // r: root location n: node total
 } PTree;
